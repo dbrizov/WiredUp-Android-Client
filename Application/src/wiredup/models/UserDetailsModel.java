31d225c696dfc51a22aaa7046a5ee4f2f1494d23
Denis Rizov <d.b.rizov@gmail.com>
2013-11-08 15:23:32 +0200

    UserDetailsModel modified
    I removed the skills, projects and certificates. They will be on
    separate pages

--- Application/src/wiredup/models/UserDetailsModel.java
+++ Application/src/wiredup/models/UserDetailsModel.java
@@ -1,16 +1,11 @@
 package wiredup.models;
 
-import java.util.List;
-
 public class UserDetailsModel {
 	private String displayName;
 	private String email;
 	private byte[] photo;
 	private String country;
-	private List<String> skills;
 	private String languages;
-	private List<CertificateModel> certificates;
-	private List<ProjectModel> projects;
 	
 	public String getDisplayName() {
 		return this.displayName;
@@ -50,29 +45,5 @@ public class UserDetailsModel {
 	
 	public void setLanguages(String languages) {
 		this.languages = languages;
-	}
-
-	public List<String> getSkills() {
-		return this.skills;
-	}
-
-	public void setSkills(List<String> skills) {
-		this.skills = skills;
-	}
-
-	public List<CertificateModel> getCertificates() {
-		return this.certificates;
-	}
-
-	public void setCertificates(List<CertificateModel> certificates) {
-		this.certificates = certificates;
-	}
-
-	public List<ProjectModel> getProjects() {
-		return this.projects;
-	}
-
-	public void setProjects(List<ProjectModel> projects) {
-		this.projects = projects;
 	}
 }
