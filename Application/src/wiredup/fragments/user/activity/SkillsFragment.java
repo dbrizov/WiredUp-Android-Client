660579047aafdd8bb1c71797ffe275961eb1c358
Denis Rizov <d.b.rizov@gmail.com>
2013-11-14 12:01:25 +0200

    UserActivity -> SkillsFragment implemented

--- Application/src/wiredup/fragments/user/activity/SkillsFragment.java
+++ Application/src/wiredup/fragments/user/activity/SkillsFragment.java
@@ -1,14 +1,35 @@
 package wiredup.fragments.user.activity;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import wiredup.adapters.UserActivitySkillsAdapter;
+import wiredup.client.R;
+import wiredup.http.IOnError;
+import wiredup.http.IOnSuccess;
+import wiredup.models.SkillModel;
 import wiredup.utils.BundleKeys;
+import wiredup.utils.ErrorNotifier;
+import wiredup.utils.WiredUpApp;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 public class SkillsFragment extends Fragment {
 	private int userId;
+	private List<SkillModel> skills;
+	private UserActivitySkillsAdapter skillsAdapter;
+	private boolean isDataLoaded;
+
+	private ListView listViewSkills;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -16,11 +37,69 @@ public class SkillsFragment extends Fragment {
 
 		Bundle bundle = this.getArguments();
 		this.userId = bundle.getInt(BundleKeys.USER_ID);
+
+		this.skills = new ArrayList<SkillModel>();
+		this.isDataLoaded = false;
 	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		return super.onCreateView(inflater, container, savedInstanceState);
+
+		View rootLayoutView = inflater.inflate(
+				R.layout.fragment_skills_user_activity, container, false);
+
+		// Find the listView
+		this.listViewSkills = (ListView) rootLayoutView.findViewById(R.id.listView_skills);
+		
+		// Set up the listView
+		if (!this.isDataLoaded) {
+			this.getDataFromServerAndSetUpListView();
+		} else {
+			this.setUpListView();
+		}
+
+		return rootLayoutView;
+	}
+
+	private void getDataFromServerAndSetUpListView() {
+		IOnSuccess onSuccess = new IOnSuccess() {
+			@Override
+			public void performAction(String data) {
+				SkillsFragment.this.loadSkillsData(data);
+				SkillsFragment.this.setUpListView();
+			}
+		};
+		
+		IOnError onError = new IOnError() {
+			@Override
+			public void performAction(String data) {
+				ErrorNotifier.displayErrorMessage(
+						SkillsFragment.this.getActivity(), data);
+			}
+		};
+		
+		WiredUpApp
+				.getData()
+				.getSkills()
+				.getAllForUser(this.userId, WiredUpApp.getSessionKey(),
+						onSuccess, onError);
+	}
+	
+	private void loadSkillsData(String data) {
+		Gson gson = new Gson();
+		Type listType = new TypeToken<List<SkillModel>>() {}.getType();
+
+		this.skills = gson.fromJson(data, listType);
+		this.isDataLoaded = true;
+
+		Log.d("debug", "Skills Loaded");
+	}
+	
+	private void setUpListView() {
+		this.skillsAdapter = new UserActivitySkillsAdapter(this.getActivity(),
+				R.layout.list_row_skill_user_activity, this.skills);
+		
+		this.listViewSkills.setAdapter(this.skillsAdapter);
 	}
 }
