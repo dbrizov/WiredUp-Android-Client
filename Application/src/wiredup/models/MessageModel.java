3add6f4895a8178887b3e03484cfea617227fcf0
Denis Rizov <d.b.rizov@gmail.com>
2013-11-15 19:03:05 +0200

    MessageModel added

---
+++ Application/src/wiredup/models/MessageModel.java
@@ -0,0 +1,55 @@
+package wiredup.models;
+
+import java.util.Date;
+
+public class MessageModel {
+	private int id;
+	private String content;
+	private String senderName;
+	private String receiverName;
+	private Date postDate;
+	
+	public MessageModel() {
+		
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return this.content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getSenderName() {
+		return this.senderName;
+	}
+
+	public void setSenderName(String senderName) {
+		this.senderName = senderName;
+	}
+
+	public String getReceiverName() {
+		return this.receiverName;
+	}
+
+	public void setReceiverName(String receiverName) {
+		this.receiverName = receiverName;
+	}
+
+	public Date getPostDate() {
+		return this.postDate;
+	}
+
+	public void setPostDate(Date postDate) {
+		this.postDate = postDate;
+	}
+}
