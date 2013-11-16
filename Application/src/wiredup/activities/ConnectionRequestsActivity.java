e2a344511cb42df5793da45a9a2f8c5bb04cb046
Denis Rizov <d.b.rizov@gmail.com>
2013-11-16 18:17:43 +0200

    ConnectionRequestsActivity added

---
+++ Application/src/wiredup/activities/ConnectionRequestsActivity.java
@@ -0,0 +1,12 @@
+package wiredup.activities;
+
+import wiredup.client.R;
+import android.os.Bundle;
+
+public class ConnectionRequestsActivity extends OptionsMenuActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_connection_requests);
+	}
+}
