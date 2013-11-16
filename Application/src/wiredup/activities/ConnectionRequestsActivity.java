02b78af590dee0eb639daab660455a8d5ad75771
Denis Rizov <d.b.rizov@gmail.com>
2013-11-16 20:09:06 +0200

    Visualization of connection requests implemented

--- Application/src/wiredup/activities/ConnectionRequestsActivity.java
+++ Application/src/wiredup/activities/ConnectionRequestsActivity.java
@@ -1,14 +1,19 @@
 package wiredup.activities;
 
+import java.lang.reflect.Type;
 import java.util.List;
 
+import wiredup.adapters.ConnectionRequestsAdapter;
 import wiredup.client.R;
 import wiredup.models.ConnectionRequestModel;
+import wiredup.utils.BundleKey;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 public class ConnectionRequestsActivity extends OptionsMenuActivity {
-	private String connectionRequestsAsJsonString;
 	private List<ConnectionRequestModel> connectionRequests;
 	
 	private ListView listViewConnectionRequests;
@@ -18,6 +23,31 @@ public class ConnectionRequestsActivity extends OptionsMenuActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_connection_requests);
 		
-		this.listViewConnectionRequests = (ListView) this.findViewById(R.id.listView_connectionRequests);
+		String connectionRequestsAsJsonString =
+				this.getIntent().getExtras().getString(BundleKey.CONNECTION_REQUESTS);
+		
+		this.connectionRequests =
+				this.parseConnectionRequestsJsonString(connectionRequestsAsJsonString);
+		
+		this.listViewConnectionRequests =
+				(ListView) this.findViewById(R.id.listView_connectionRequests);
+		
+		this.setUpListView();
+	}
+	
+	private void setUpListView() {
+		ConnectionRequestsAdapter adapter =
+				new ConnectionRequestsAdapter(
+						this, R.layout.list_row_connection_requests, this.connectionRequests);
+		
+		this.listViewConnectionRequests.setAdapter(adapter);
+	}
+	
+	private List<ConnectionRequestModel> parseConnectionRequestsJsonString(String connectionRequestsJsonString) {
+		Gson gson = new Gson();
+		Type listType = new TypeToken<List<ConnectionRequestModel>>() {}.getType();
+
+		List<ConnectionRequestModel> connectionRequests = gson.fromJson(connectionRequestsJsonString, listType);
+		return connectionRequests;
 	}
 }
