27bf5f6c7bc947df9b5492403d4c6b640ba37f8f
Denis Rizov <d.b.rizov@gmail.com>
2013-11-16 19:26:28 +0200

    Layout for the connection requests list view added
    1) Added some icons for Accept and Cancel buttons
    2) Created xml selectors for the buttons

--- Application/src/wiredup/activities/ConnectionRequestsActivity.java
+++ Application/src/wiredup/activities/ConnectionRequestsActivity.java
@@ -1,12 +1,23 @@
 package wiredup.activities;
 
+import java.util.List;
+
 import wiredup.client.R;
+import wiredup.models.ConnectionRequestModel;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class ConnectionRequestsActivity extends OptionsMenuActivity {
+	private String connectionRequestsAsJsonString;
+	private List<ConnectionRequestModel> connectionRequests;
+	
+	private ListView listViewConnectionRequests;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_connection_requests);
+		
+		this.listViewConnectionRequests = (ListView) this.findViewById(R.id.listView_connectionRequests);
 	}
 }
